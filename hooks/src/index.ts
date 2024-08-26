2c8d47c195eab0edb33077a2fda23c54662f395f
YuvRaj6392 <74025229+YuvRaj6392@users.noreply.github.com>
2024-08-26 19:33:46 +0530

    initial commit

---
+++ hooks/src/index.ts
@@ -0,0 +1,41 @@
+import express from "express";
+const app = express();
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
+
+app.use(express.json())
+
+//https://hooks.zapier.com/hooks/catch/19896975/26gxnc2/
+//password logic
+
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
+  const userId = req.params.userId;
+  const zapId = req.params.zapId;
+  const body=req.body;
+
+  //store in db a new trigger
+  await client.$transaction(async (tx) => {
+    const run = await tx.zapRun.create({
+      data: {
+        zapId: zapId,
+        metadata:body
+      },
+    });
+    await tx.zapRunOutbox.create({
+      data:{
+        zapRunId:run.id
+      }
+    })
+  });
+
+  res.json({
+    msg:'webhook received'
+  })
+
+  //push it in to a queue (kafka/redis)
+});
+
+app.listen(3000,()=>{
+  console.log('server is listening at 3000')
+})
